a817dbc2568dcc24659aca3e3a03e355c8882876
Round0000 <billybinche@gmail.com>
2022-02-02 22:21:18 +0100

    commit

--- app.js
+++ app.js
@@ -19,3 +19,17 @@ document.addEventListener("click", (e) => {
     e.target.closest(".appWindow").classList.toggle("maximized");
   }
 });
+
+
+// Drag'n'drop
+
+document.addEventListener('mousedown', e => {
+  if (e.target.classList.contains('appWindowHeader')) {
+    console.log('%capp.js line:29 e.target', 'color: #007acc;', e);
+    e.target.screenX = 20;
+    
+  }
+})
+
+function dragndrop(el) {
+}
