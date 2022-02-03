a2986b53112f37cc168c09f5ed2ae04d36bdcbbf
Round0000 <billybinche@gmail.com>
2022-02-03 02:24:51 +0100

    commit

--- app.js
+++ app.js
@@ -20,16 +20,67 @@ document.addEventListener("click", (e) => {
   }
 });
 
-
 // Drag'n'drop
 
-document.addEventListener('mousedown', e => {
-  if (e.target.classList.contains('appWindowHeader')) {
-    console.log('%capp.js line:29 e.target', 'color: #007acc;', e);
-    e.target.screenX = 20;
-    
-  }
-})
+const appWindow = document.querySelector(".appWindow");
 
-function dragndrop(el) {
+let dragged = false;
+
+document.addEventListener("mousedown", (e) => {
+  if (!e.target.classList.contains("appWindowHeader")) return;
+
+  dragged = true;
+});
+
+document.addEventListener("mouseup", (e) => {
+  dragged = false;
+});
+
+document.addEventListener("mousemove", (e) => {
+  if (!dragged) return;
+
+  dragndrop(appWindow, e);
+});
+
+function dragndrop(el, e) {
+  let currX = parseInt(window.getComputedStyle(el).left);
+  let currY = parseInt(window.getComputedStyle(el).top);
+
+  if (el.getBoundingClientRect().left < 0) {
+    currX = 0;
+  } else {
+    currX += e.movementX;
+  }
+  if (el.getBoundingClientRect().top < 0) {
+    currY = 0;
+  } else {
+    currY += e.movementY;
+  }
+
+  currY += e.movementY;
+  el.style.left = currX + "px";
+  el.style.top = currY + "px";
 }
+
+// Right click
+
+document.addEventListener("contextmenu", (e) => {
+  e.preventDefault();
+
+  console.log(e);
+
+  console.log("%capp.js line:70 e.x", "color: #007acc;", e.x);
+
+  const contextualMenu =
+    e.target.parentElement.querySelector(".contextualMenu");
+
+  contextualMenu.style.left = e.x + "px";
+  contextualMenu.style.top = e.y + "px";
+  contextualMenu.classList.remove("dnone");
+});
+
+document.addEventListener("click", (e) => {
+  if (document.querySelector(".contextualMenu")) {
+    document.querySelector(".contextualMenu").classList.add("dnone");
+  }
+});
