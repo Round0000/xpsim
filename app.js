9eb4896248b400e3ac9479eedb1593ed07286a88
Round0000 <billybinche@gmail.com>
2022-02-04 14:25:35 +0100

    commit

--- app.js
+++ app.js
@@ -26,25 +26,25 @@ const apps = {
   },
   fileexplorer: {
     dataid: "fileexplorer",
-    title: "File Explorer",
+    title: "My Documents",
     menu: true,
     menuItems: ["File", "Edit", "View", "Favorites", "Tools", "Help"],
     icon: "./assets/icons/fileexplorer.ico",
     html: `
     <div class="fileExpActions">
-    <button><img src="./assets/icons/help.ico" alt="">Back</button>
-    <button>Forward</button>
-    <button>Prev Folder</button>
+    <button><img src="./assets/icons/back.ico" alt="">Back</button>
+    <button><img src="./assets/icons/forward.ico" alt=""></button>
+    <button><img src="./assets/icons/prevfolder.ico" alt=""></button>
     <div class="verticalseparator"></div>
     <button><img src="./assets/icons/search.ico" alt="">Search</button>
     <button><img src="./assets/icons/folders.ico" alt="">Folders</button>
     <div class="verticalseparator"></div>
-    <button><img src="./assets/icons/display.ico" alt="">Display</button>
+    <button><img src="./assets/icons/display.ico" alt=""></button>
   </div>
   <div class="fileExpAddressBar">
     <span>Address</span>
     <div class="fileExpAddressInput"><img src="./assets/icons/mydocuments.ico" alt="" />
-      <input type="text" value="My Documents">
+      <input spellcheck="false" type="text" value="My Documents">
     </div>
     <button>Go</button>
   </div>
@@ -75,7 +75,7 @@ const apps = {
         </div>
         <div class="sidebarBlockItems">
           <button class="sidebarItem">
-            <img src="./assets/icons/makenewfolder.ico" alt="">
+            <img src="./assets/icons/desktop.ico" alt="">
             Desktop
           </button>
           <button class="sidebarItem">
@@ -116,18 +116,28 @@ const apps = {
     </div>
   </div>
     `,
-  }
-}
+  },
+  calculator: {
+    dataid: "calculator",
+    title: "Calculator Plus",
+    menu: true,
+    menuItems: ["Edit", "View", "Help"],
+    icon: "./assets/icons/calculator.ico",
+    html: `
+
+    `,
+  },
+};
 
 startBtn.addEventListener("click", () => {
   startMenu.classList.toggle("dnone");
 });
 
-document.addEventListener('click', e => {
-  if (e.target.closest('#startMenu') || e.target.closest('#startBtn')) return;
+document.addEventListener("click", (e) => {
+  if (e.target.closest("#startMenu") || e.target.closest("#startBtn")) return;
 
-  startMenu.classList.add('dnone');
-})
+  startMenu.classList.add("dnone");
+});
 
 bsodToggle.addEventListener("click", () => {
   windows.classList.add("bsod");
@@ -148,10 +158,22 @@ document.addEventListener("click", (e) => {
   if (e.target.classList.contains("btnWindowClose")) {
     const app = e.target.closest(".appWindow");
     app.classList.add("dnone");
-    taskbar.querySelector(`.openedAppTab[data-app="${app.dataset.app}"]`).remove();
-    if (taskbar.querySelector('.openedAppTab')) {
-      setActiveWindow(document.querySelector('.appWindow:last-of-type'));
+    taskbar
+      .querySelector(`.openedAppTab[data-app="${app.dataset.app}"]`)
+      .remove();
+    if (taskbar.querySelector(".openedAppTab")) {
+      setActiveWindow(document.querySelector(".appWindow:last-of-type"));
     }
+  }
+  if (e.target.classList.contains("btnWindowMinimize")) {
+    e.target.closest(".appWindow").classList.toggle("minimized");
+    setTimeout(() => {
+      openedApps
+      .querySelector(
+        `[data-app="${e.target.closest(".appWindow").dataset.app}"]`
+      )
+      .classList.remove("active");
+    }, 100);
   }
 });
 
@@ -204,86 +226,89 @@ function dragndrop(el, e) {
 document.addEventListener("contextmenu", (e) => {
   e.preventDefault();
 
-
-
   contextualMenu.style.left = e.x + "px";
   contextualMenu.style.top = e.y + "px";
-  contextualMenu.classList.remove("dnone");
+  setTimeout(() => {
+    contextualMenu.classList.remove("invisible");
+  }, 100);
 });
 
 document.addEventListener("click", (e) => {
-  contextualMenu.classList.add("dnone");
-
+  setTimeout(() => {
+    contextualMenu.classList.add("invisible");
+  }, 100);
 });
 
-
-document.addEventListener("dblclick", e => {
-  
-
-  if (e.target.parentElement.classList.contains('appIcon')) {
+document.addEventListener("dblclick", (e) => {
+  if (e.target.parentElement.classList.contains("appIcon")) {
     openApp(e.target.parentElement.dataset.app);
   }
-})
+});
 
 // Switch active window
-document.querySelectorAll('.appWindow').forEach(app => {
-  app.addEventListener('click', e => {
+document.querySelectorAll(".appWindow").forEach((app) => {
+  app.addEventListener("click", (e) => {
     setActiveWindow(app);
-  })
-})
+  });
+});
 
 function setActiveWindow(el) {
-  if (document.querySelector('.activeWindow')) {
-    document.querySelector('.activeWindow').classList.remove('activeWindow');
+  if (document.querySelector(".activeWindow")) {
+    document.querySelector(".activeWindow").classList.remove("activeWindow");
   }
 
-  el.classList.add('activeWindow');
+  el.classList.add("activeWindow");
 
   if (taskbar.querySelector(".openedAppTab.active")) {
-    taskbar.querySelector(".openedAppTab.active").classList.remove('active');
+    taskbar.querySelector(".openedAppTab.active").classList.remove("active");
   }
 
-  taskbar.querySelector(`[data-app="${el.dataset.app}"]`).classList.add('active');
+  taskbar
+    .querySelector(`[data-app="${el.dataset.app}"]`)
+    .classList.add("active");
 }
 
 // Taskbar shortcuts
-document.querySelector('.scutDesktop').addEventListener("click", e => {
-  document.querySelectorAll('.appWindow').forEach(app => {
-    app.classList.add('dnone');
-  })
-})
+document.querySelector(".scutDesktop").addEventListener("click", (e) => {
+  document.querySelectorAll(".appWindow").forEach((app) => {
+    app.classList.add("dnone");
+  });
+});
 
 function openApp(app) {
   const a = apps[app];
 
-  const newApp = document.createElement('DIV');
-  newApp.classList.add('appWindow');
+  const newApp = document.createElement("DIV");
+  newApp.classList.add("appWindow");
   newApp.dataset.app = a.dataid;
   newApp.dataset.appTitle = a.title;
   newApp.innerHTML = appTemplate;
-  newApp.querySelector('.appWindowHeader img').src = a.icon;
-  newApp.querySelector('.appTitle').innerText = a.title;
+  newApp.querySelector(".appWindowHeader img").src = a.icon;
+  newApp.querySelector(".appTitle").innerText = a.title;
   if (a.menu) {
-    const menu = newApp.querySelector('.appMenu');
-    a.menuItems.forEach(item => {
-      const li = document.createElement('LI');
+    const menu = newApp.querySelector(".appMenu");
+    a.menuItems.forEach((item) => {
+      const li = document.createElement("LI");
       li.innerText = item;
       menu.append(li);
       menu.classList.remove("dnone");
-    })
+    });
   }
 
-  newApp.querySelector('.appContent').innerHTML = a.html;
+  newApp.querySelector(".appContent").innerHTML = a.html;
 
   desktop.append(newApp);
 
-  
-  const newTab = document.createElement('BUTTON');
-  newTab.classList.add('openedAppTab');
+  const newTab = document.createElement("BUTTON");
+  newTab.classList.add("openedAppTab");
   newTab.dataset.app = a.dataid;
   newTab.innerHTML = `<img src="${a.icon}"" alt="" />${a.title}`;
   openedApps.append(newTab);
-  
+
   setActiveWindow(newApp);
 }
 
+//
+//
+//
+// openApp("calculator");
