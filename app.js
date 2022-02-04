868c42e65fd846da17e37f2be87a1e830eee1849
Round0000 <billybinche@gmail.com>
2022-02-04 16:16:07 +0100

    commit

--- app.js
+++ app.js
@@ -1,134 +1,3 @@
-const appTemplate = `
-  <header class="appWindowHeader">
-    <img src="" alt="" />
-    <p class="appTitle"></p>
-    <div class="appWindowActions">
-      <button class="btnWindowMinimize"></button>
-      <button class="btnWindowMaximize"></button>
-      <button class="btnWindowClose"></button>
-    </div>
-  </header>
-  <ul class="appMenu dnone">
-  </ul>
-  <div class="appContent"></div>
-`;
-
-const apps = {
-  notepad: {
-    dataid: "notepad",
-    title: "Notepad",
-    menu: true,
-    menuItems: ["File", "Edit", "Format", "View", "Help"],
-    icon: "./assets/icons/notepad.ico",
-    html: `
-    <textarea class="notepadTextarea" spellcheck="false"></textarea>
-    `,
-  },
-  fileexplorer: {
-    dataid: "fileexplorer",
-    title: "My Documents",
-    menu: true,
-    menuItems: ["File", "Edit", "View", "Favorites", "Tools", "Help"],
-    icon: "./assets/icons/fileexplorer.ico",
-    html: `
-    <div class="fileExpActions">
-    <button><img src="./assets/icons/back.ico" alt="">Back</button>
-    <button><img src="./assets/icons/forward.ico" alt=""></button>
-    <button><img src="./assets/icons/prevfolder.ico" alt=""></button>
-    <div class="verticalseparator"></div>
-    <button><img src="./assets/icons/search.ico" alt="">Search</button>
-    <button><img src="./assets/icons/folders.ico" alt="">Folders</button>
-    <div class="verticalseparator"></div>
-    <button><img src="./assets/icons/display.ico" alt=""></button>
-  </div>
-  <div class="fileExpAddressBar">
-    <span>Address</span>
-    <div class="fileExpAddressInput"><img src="./assets/icons/mydocuments.ico" alt="" />
-      <input spellcheck="false" type="text" value="My Documents">
-    </div>
-    <button>Go</button>
-  </div>
-  <div class="appWindowContent">
-    <aside class="sidebar">
-      <div class="sidebarBlock">
-        <div class="sidebarBlockHeader">
-          File and Folder Tasks
-        </div>
-        <div class="sidebarBlockItems">
-          <button class="sidebarItem">
-            <img src="./assets/icons/makenewfolder.ico" alt="">
-            Make a new folder
-          </button>
-          <button class="sidebarItem">
-            <img src="./assets/icons/makenewfolder.ico" alt="">
-            Publish this folder to the Web
-          </button>
-          <button class="sidebarItem">
-            <img src="./assets/icons/makenewfolder.ico" alt="">
-            Share this folder
-          </button>
-        </div>
-      </div>
-      <div class="sidebarBlock">
-        <div class="sidebarBlockHeader">
-          Other places
-        </div>
-        <div class="sidebarBlockItems">
-          <button class="sidebarItem">
-            <img src="./assets/icons/desktop.ico" alt="">
-            Desktop
-          </button>
-          <button class="sidebarItem">
-            <img src="./assets/icons/makenewfolder.ico" alt="">
-            My Computer
-          </button>
-          <button class="sidebarItem">
-            <img src="./assets/icons/makenewfolder.ico" alt="">
-            My Network Places
-          </button>
-        </div>
-      </div>
-      <div class="sidebarBlock">
-        <div class="sidebarBlockHeader">
-          Details
-        </div>
-        <div class="sidebarBlockItems">
-          <div class="folderDetails">
-            <p>My Documents</p>
-            <p>System Folder</p>
-          </div>
-        </div>
-      </div>
-    </aside>
-    <div class="appWindowInside">
-      <button class="folderItem">
-        <img src="./assets/icons/mymusic.ico" alt="" class="icon_regular">
-        My Music
-      </button>
-      <button class="folderItem">
-        <img src="./assets/icons/mypictures.ico" alt="" class="icon_regular">
-        My Pictures
-      </button>
-      <button class="folderItem">
-        <img src="./assets/icons/myvideos.ico" alt="" class="icon_regular">
-        My Videos
-      </button>
-    </div>
-  </div>
-    `,
-  },
-  calculator: {
-    dataid: "calculator",
-    title: "Calculator Plus",
-    menu: true,
-    menuItems: ["Edit", "View", "Help"],
-    icon: "./assets/icons/calculator.ico",
-    html: `
-
-    `,
-  },
-};
-
 startBtn.addEventListener("click", () => {
   startMenu.classList.toggle("dnone");
 });
@@ -169,10 +38,10 @@ document.addEventListener("click", (e) => {
     e.target.closest(".appWindow").classList.toggle("minimized");
     setTimeout(() => {
       openedApps
-      .querySelector(
-        `[data-app="${e.target.closest(".appWindow").dataset.app}"]`
-      )
-      .classList.remove("active");
+        .querySelector(
+          `[data-app="${e.target.closest(".appWindow").dataset.app}"]`
+        )
+        .classList.remove("active");
     }, 100);
   }
 });
@@ -181,6 +50,8 @@ document.addEventListener("click", (e) => {
 
 let dragged = false;
 let currentlyDragged;
+let draggedOffsetX;
+let draggedOffsetY;
 
 document.addEventListener("mousedown", (e) => {
   if (!e.target.classList.contains("appWindowHeader")) return;
@@ -188,6 +59,9 @@ document.addEventListener("mousedown", (e) => {
   setActiveWindow(e.target.closest(".appWindow"));
   dragged = true;
   currentlyDragged = e.target.closest(".appWindow");
+
+  draggedOffsetX = e.offsetX;
+  draggedOffsetY = e.offsetY;
 });
 
 document.addEventListener("mouseup", (e) => {
@@ -196,29 +70,24 @@ document.addEventListener("mouseup", (e) => {
 
 document.addEventListener("mousemove", (e) => {
   if (!dragged) return;
-  console.log(e);
 
   dragndrop(currentlyDragged, e);
 });
 
 function dragndrop(el, e) {
-  let currX = parseInt(window.getComputedStyle(el).left);
-  let currY = parseInt(window.getComputedStyle(el).top);
 
-  if (el.getBoundingClientRect().left < 0) {
-    currX = 0;
+  if (el.getBoundingClientRect().left <= 0) {
+    el.style.left = "0";
   } else {
-    currX += e.movementX;
+    el.style.left = e.pageX - draggedOffsetX + "px";
+    el.style.top = e.pageY - draggedOffsetY + "px";
   }
-  if (el.getBoundingClientRect().top < 0) {
-    currY = 0;
+  if (el.getBoundingClientRect().top <= 0) {
+    el.style.top = "0";
   } else {
-    currY += e.movementY;
+    el.style.left = e.pageX - draggedOffsetX + "px";
+    el.style.top = e.pageY - draggedOffsetY + "px";
   }
-
-  currY += e.movementY;
-  el.style.left = currX + "px";
-  el.style.top = currY + "px";
 }
 
 // Right click
@@ -283,6 +152,9 @@ function openApp(app) {
   newApp.dataset.app = a.dataid;
   newApp.dataset.appTitle = a.title;
   newApp.innerHTML = appTemplate;
+  if (!a.maximize) {
+    newApp.querySelector('.btnWindowMaximize').disabled = true;
+  }
   newApp.querySelector(".appWindowHeader img").src = a.icon;
   newApp.querySelector(".appTitle").innerText = a.title;
   if (a.menu) {
@@ -307,6 +179,29 @@ function openApp(app) {
 
   setActiveWindow(newApp);
 }
+
+// Internal clock
+function getCurrentTime() {
+  let currentTime = new Date(Date.now());
+  let hours = currentTime.getHours();
+  let minutes = currentTime.getMinutes();
+
+  if (hours < 10) {
+    hours = "0" + hours;
+  }
+
+  if (minutes < 10) {
+    minutes = "0" + minutes;
+  }
+
+  time.innerText = hours + ":" + minutes;
+}
+
+getCurrentTime();
+
+setInterval(() => {
+  getCurrentTime();
+}, 30000);
 
 //
 //
---
+++ templates.js
@@ -0,0 +1,172 @@
+const appTemplate = `
+  <header class="appWindowHeader">
+    <img src="" alt="" />
+    <p class="appTitle"></p>
+    <div class="appWindowActions">
+      <button class="btnWindowMinimize"></button>
+      <button class="btnWindowMaximize"></button>
+      <button class="btnWindowClose"></button>
+    </div>
+  </header>
+  <ul class="appMenu dnone">
+  </ul>
+  <div class="appContent"></div>
+`;
+
+const apps = {
+  notepad: {
+    dataid: "notepad",
+    title: "Notepad",
+    menu: true,
+    menuItems: ["File", "Edit", "Format", "View", "Help"],
+    icon: "./assets/icons/notepad.ico",
+    maximize: true,
+    html: `
+    <textarea class="notepadTextarea" spellcheck="false"></textarea>
+    `,
+  },
+  fileexplorer: {
+    dataid: "fileexplorer",
+    title: "My Documents",
+    menu: true,
+    menuItems: ["File", "Edit", "View", "Favorites", "Tools", "Help"],
+    icon: "./assets/icons/fileexplorer.ico",
+    maximize: true,
+    html: `
+    <div class="fileExpActions">
+    <button><img src="./assets/icons/back.ico" alt="">Back</button>
+    <button><img src="./assets/icons/forward.ico" alt=""></button>
+    <button><img src="./assets/icons/prevfolder.ico" alt=""></button>
+    <div class="verticalseparator"></div>
+    <button><img src="./assets/icons/search.ico" alt="">Search</button>
+    <button><img src="./assets/icons/folders.ico" alt="">Folders</button>
+    <div class="verticalseparator"></div>
+    <button><img src="./assets/icons/display.ico" alt=""></button>
+  </div>
+  <div class="fileExpAddressBar">
+    <span>Address</span>
+    <div class="fileExpAddressInput"><img src="./assets/icons/mydocuments.ico" alt="" />
+      <input spellcheck="false" type="text" value="My Documents">
+    </div>
+    <button>Go</button>
+  </div>
+  <div class="appWindowContent">
+    <aside class="sidebar">
+      <div class="sidebarBlock">
+        <div class="sidebarBlockHeader">
+          File and Folder Tasks
+        </div>
+        <div class="sidebarBlockItems">
+          <button class="sidebarItem">
+            <img src="./assets/icons/makenewfolder.ico" alt="">
+            Make a new folder
+          </button>
+          <button class="sidebarItem">
+            <img src="./assets/icons/makenewfolder.ico" alt="">
+            Publish this folder to the Web
+          </button>
+          <button class="sidebarItem">
+            <img src="./assets/icons/makenewfolder.ico" alt="">
+            Share this folder
+          </button>
+        </div>
+      </div>
+      <div class="sidebarBlock">
+        <div class="sidebarBlockHeader">
+          Other places
+        </div>
+        <div class="sidebarBlockItems">
+          <button class="sidebarItem">
+            <img src="./assets/icons/desktop.ico" alt="">
+            Desktop
+          </button>
+          <button class="sidebarItem">
+            <img src="./assets/icons/makenewfolder.ico" alt="">
+            My Computer
+          </button>
+          <button class="sidebarItem">
+            <img src="./assets/icons/makenewfolder.ico" alt="">
+            My Network Places
+          </button>
+        </div>
+      </div>
+      <div class="sidebarBlock">
+        <div class="sidebarBlockHeader">
+          Details
+        </div>
+        <div class="sidebarBlockItems">
+          <div class="folderDetails">
+            <p>My Documents</p>
+            <p>System Folder</p>
+          </div>
+        </div>
+      </div>
+    </aside>
+    <div class="appWindowInside">
+      <button class="folderItem">
+        <img src="./assets/icons/mymusic.ico" alt="" class="icon_regular">
+        My Music
+      </button>
+      <button class="folderItem">
+        <img src="./assets/icons/mypictures.ico" alt="" class="icon_regular">
+        My Pictures
+      </button>
+      <button class="folderItem">
+        <img src="./assets/icons/myvideos.ico" alt="" class="icon_regular">
+        My Videos
+      </button>
+    </div>
+  </div>
+    `,
+  },
+  calculator: {
+    dataid: "calculator",
+    title: "Calculator",
+    menu: true,
+    menuItems: ["Edit", "View", "Help"],
+    icon: "./assets/icons/calculator.ico",
+    maximize: false,
+    html: `
+    <p class="calc_result">0.</p>
+            <div class="calc_interface">
+              <div class="calc_empty"></div>
+
+              <div class="calc_topline">
+                <button>Backspace</button>
+                <button>CE</button>
+                <button>C</button>
+              </div>
+
+              <div class="calc_leftline">
+                <button>MC</button>
+                <button>MR</button>
+                <button>MS</button>
+                <button>M+</button>
+              </div>
+
+              <div class="calc_mainblock">
+                <button>7</button>
+                <button>8</button>
+                <button>9</button>
+                <button class="calc_red">/</button>
+                <button>sqrt</button>
+                <button>4</button>
+                <button>5</button>
+                <button>6</button>
+                <button class="calc_red">*</button>
+                <button>%</button>
+                <button>1</button>
+                <button>2</button>
+                <button>3</button>
+                <button class="calc_red">-</button>
+                <button>1/x</button>
+                <button>0</button>
+                <button>+/-</button>
+                <button>.</button>
+                <button class="calc_red">+</button>
+                <button class="calc_red">=</button>
+              </div>
+            </div>
+    `,
+  },
+};
