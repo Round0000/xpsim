8523e250ee81c60b4a618cdc7374160f9e79addd
Round0000 <billybinche@gmail.com>
2022-02-07 16:14:43 +0100

    commit

--- apps/internetexplorer.js
+++ apps/internetexplorer.js
@@ -3,4 +3,5 @@ const appW = document.querySelector('.appWindow[data-app="internetexplorer"]');
 appW.querySelector(".ie_urlform").addEventListener("submit", (e) => {
   e.preventDefault();
   console.log(e.srcElement[0].value);
+  
 });
