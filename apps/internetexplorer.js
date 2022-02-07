2feaff9f84483593a62324e1773a1cfe624773fd
Round0000 <billybinche@gmail.com>
2022-02-07 18:02:56 +0100

    commit

--- apps/internetexplorer.js
+++ apps/internetexplorer.js
@@ -3,5 +3,30 @@ const appW = document.querySelector('.appWindow[data-app="internetexplorer"]');
 appW.querySelector(".ie_urlform").addEventListener("submit", (e) => {
   e.preventDefault();
   console.log(e.srcElement[0].value);
-  
+});
+
+appW.addEventListener("click", (e) => {
+  if (
+    e.target.classList.contains("ie_tab") &&
+    !e.target.classList.contains("ie_tab_new")
+  ) {
+    appW
+      .querySelector(".ie_tab:not(.ie_tab_inactive)")
+      .classList.add("ie_tab_inactive");
+    e.target.classList.remove("ie_tab_inactive");
+  }
+
+  if (e.target.classList.contains("ie_tab_new")) {
+    const newTab = document.createElement("LI");
+    appW
+      .querySelector(".ie_tab:not(.ie_tab_inactive)")
+      .classList.add("ie_tab_inactive");
+    newTab.classList.add("ie_tab");
+    newTab.innerText = "New Tab";
+    e.target.remove();
+    appW.querySelector(".ie_tabs").append(newTab);
+    const newTabTab = document.createElement("LI");
+    newTabTab.classList.add("ie_tab", "ie_tab_inactive", "ie_tab_new");
+    appW.querySelector(".ie_tabs").append(newTabTab);
+  }
 });
